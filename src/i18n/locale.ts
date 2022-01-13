6d901f79e3511e4ad9d224cc264d2c64ca156514
Curt Grimes <curt@curtgrimes.com>
2022-01-13 08:37:42 -0500

    Add Portuguese localization

--- src/i18n/locale.ts
+++ src/i18n/locale.ts
@@ -1,10 +1,12 @@
 // originally from https://github.com/valentine195/obsidian-leaflet-plugin/blob/master/src/l10n/locale.ts
 import en from "./locales/en";
+import pt from "./locales/pt";
 
 const locale = window.moment.locale;
 
 const localeMap: { [k: string]: Partial<typeof en> } = {
 	en,
+	pt,
 };
 
 const userLocale = localeMap[locale()];
---
+++ src/i18n/locales/pt.ts
@@ -0,0 +1,11 @@
+export default {
+	Mute: "Silenciar",
+	Unmute: "Ouvir",
+	"Stop metronome": "Pare o metrônomo",
+	"Start metronome": "Iniciar o metrônomo",
+	"%1 BPM": "%1 BPM",
+	"Invalid BPM": "BPM inválido",
+	"Invalid or unsupported meter": "Métrica inválida ou não suportada",
+	"Stopping in %1...": "Parando em %1...",
+	Cancel: "Cancelar",
+};
